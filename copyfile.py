74c1fa3720fb15eb2fae7283d0788ba65cd1bb4d
RenHao116 <31802578+RenHao116@users.noreply.github.com>
2017-11-03 21:05:27 -0500

    Add files via upload
    Changing urllib to urllib3

---
+++ copyfile.py
@@ -0,0 +1,25 @@
+import shutil, os,urllib.request
+a=input('What home work is this?\n')
+urllib.request.urlretrieve ("http://research.engineering.wustl.edu/~bmoseley/CSE240-FA17/HW/hw_"+a+".tex", "hw_"+a+".tex")
+shutil.copytree(r'Homework',r'Homework '+a)
+i=1
+for folderName,subfolders,filenames in os.walk('Homework '+a):
+	print('The current folder is ' + folderName)
+	if folderName==r'Homework '+a+"\HW":
+		os.rename(folderName,r'Homework '+a+"\HW"+a)
+		print("renamed")
+	if folderName==r'Homework '+a+"\HW P"+str(i):
+		print("renamed")
+		os.rename(folderName, r'Homework '+a+"\HW"+a+" P"+str(i))
+	for subfolder in subfolders:
+		print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+			
+		print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+	for filename in filenames:
+		if filename=="HW P.tex":
+			os.rename(r'Homework '+a+"\HW"+a+" P"+str(i)+"\HW P.tex",r'Homework '+a+"\HW"+a+" P"+str(i)+"\HW"+a+" P"+str(i)+".tex")
+			i+=1
+		print('FILE INSIDE ' + folderName + ': '+ filename)
+
+	print('')
+shutil.move(r'hw_'+a+'.tex',r'Homework '+a+'\HW'+a)
